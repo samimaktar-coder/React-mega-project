9eae7725dc5639bce49aee3570e021c25e375a31
Samim Aktar <samimaktar94320@gmail.com>
2023-11-25 11:05:24 +0530

    Minor update

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import Header from "./header/header";
+import Header from './header/Header'
 import Footer from "./footer/Footer";
 import Container from "./container/Container";
 import Logo from './Logo';
