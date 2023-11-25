2611ff8c89cf031d3515290f22cff1048a5abe3f
Samim Aktar <samimaktar94320@gmail.com>
2023-11-25 09:57:22 +0530

    All file added

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./header/header";
+import Footer from "./footer/Footer";
+import Container from "./container/Container";
+import Logo from './Logo';
+import Button from './Button';
+import Input from './Input';
+import LogoutBtn from "./header/LogoutBtn";
+import Select from "./Select";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostCard from './PostCard';
+import PostForm from './post-form/PostForm';
+import AuthLayout from './AuthLayout'
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    Input,
+    Select,
+    RTE,
+    Signup,
+    Login,
+    PostCard,
+    PostForm,
+    AuthLayout
+};
